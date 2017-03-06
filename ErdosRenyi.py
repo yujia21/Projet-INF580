abe73a31589c9e66ad80c9ed276ea694b17ac0c9
yujia21 <odonut@gmail.com>
2017-03-06 11:25:26 +0100

    Updated graph generator
    Random weights

--- ErdosRenyi.py
+++ ErdosRenyi.py
@@ -6,4 +6,7 @@ import numpy as np
 for n in np.arange(10, 100, 10):
     for p in np.arange(0.3, 0.9, 0.2):
         G=nx.erdos_renyi_graph(n,p)
-        nx.write_edgelist(G, 'data_'+str(n)+'_'+str(p)+'.edgelist')
+        for (i, j) in G.edges() : 
+            #random weight for each edge between 0 to 5
+            G[i][j]['weight'] = int(np.ceil(np.random.uniform(0,5)))
+        nx.write_weighted_edgelist(G, 'data_'+str(n)+'_'+str(p)+'.edgelist')
